151ea73b82d46aaf80a3240da58681e3f587cc1b
Brendan Freeman <bfreeman0@gmail.com>
2024-01-31 18:03:02 +0000

    added npmignore

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import dropDownMenu from "./dropDownMenu/dropDownMenu";
 import mobileMenu from "./mobileMenu/mobileMenu";
 
-const app = document.querySelector('.app');
+// const app = document.querySelector('.app');
 
 mobileMenu(app, 2, "Trees")
 
-dropDownMenu(app, "Menu", "#FFFFFF", [{"link": "https://gameroracle.com", "name": "gamer oracle"}], 'flat');
+// dropDownMenu(app, "Menu", "#FFFFFF", [{"link": "https://gameroracle.com", "name": "gamer oracle"}], 'flat');
 
-dropDownMenu(app, "Another button", "red", [{
-  "link": "google.com",
-  "name": "The Google"
-}], "shadow")
+// dropDownMenu(app, "Another button", "red", [{
+//   "link": "google.com",
+//   "name": "The Google"
+// }], "shadow")
 
